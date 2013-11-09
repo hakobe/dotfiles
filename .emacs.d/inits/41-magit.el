1c1be349ec6de8e460dad41482c26e6643b52f70
hakobe <hakobe@gmail.com>
2013-11-09 13:19:19 +0900

    magit-statusでstashを表示しない

--- .emacs.d/inits/41-magit.el
+++ .emacs.d/inits/41-magit.el
@@ -1,2 +1,9 @@
 (el-get 'sync 'magit)
 (require 'magit)
+
+(defun magit-init ()
+  ; stashはたくさんためるので表示しない
+  (remove-hook 'magit-status-sections-hook 'magit-insert-stashes))
+
+(add-hook 'magit-mode-hook
+          '(lambda () (magit-init)))
