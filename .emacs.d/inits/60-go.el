c99681e22f1eb7eb7f15965584440450b0808570
hakobe <hakobe@gmail.com>
2013-11-10 01:10:37 +0900

    go complete setting

--- .emacs.d/inits/60-go.el
+++ .emacs.d/inits/60-go.el
@@ -1,6 +1,21 @@
-(el-get 'sync 'go-mode)
+(add-to-list 'el-get-sources
+    '(:name go-autocomplete
+           :description "An autocompletion daemon for the Go programming language"
+           :type github
+           :pkgname "nsf/gocode"
+           :depends (auto-complete)
+           :features (go-autocomplete)
+           :build (("go" "build" "-o" "gocode"))
+           :build/windows-nt (("go" "build" "-o" "gocode.exe"))
+           :load-path ("emacs")
+           :prepare (progn
+                      (add-to-list 'exec-path (el-get-package-directory "go-autocomplete")))))
+
+(el-get 'sync '(go-mode go-autocomplete))
 (add-hook 'go-mode-hook
           '(lambda ()
+            (require 'go-autocomplete)
+            (require 'auto-complete-config)
             (setq c-basic-offset 4)
             (setq indent-tabs-mode t)))
 
