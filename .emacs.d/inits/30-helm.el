51313704a63ad39eb92fad7e103efbb52f074de8
hakobe <hakobe@gmail.com>
2013-11-09 21:35:06 +0900

    helmまわり整理

--- .emacs.d/inits/30-helm.el
+++ .emacs.d/inits/30-helm.el
@@ -1,21 +1,9 @@
-(el-get 'sync 'helm)
+(el-get 'sync '(helm helm-c-yasnippet))
 
 (require 'helm)
 (require 'helm-config)
 
-(defun my-helm-mini ()
-  (interactive)
-  (require 'helm-files)
-  (helm-other-buffer '(
-                       helm-source-buffers-list
-                       helm-source-files-in-current-dir
-                       helm-source-recentf
-                       helm-source-file-cache
-                       helm-source-locate
-                       )
-                     "*helm mini*"))
-
-(global-set-key (kbd "C-;") 'my-helm-mini)
+(global-set-key (kbd "C-;") 'helm-mini)
 (global-set-key (kbd "C-:") 'helm-M-x)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
