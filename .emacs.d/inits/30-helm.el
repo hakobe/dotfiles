1fef5aac3cc65a68bffa3d1809cb4a08982e401a
hakobe <hakobe@gmail.com>
2013-11-30 15:28:55 +0900

    helmの設定

--- .emacs.d/inits/30-helm.el
+++ .emacs.d/inits/30-helm.el
@@ -1,8 +1,20 @@
 (el-get 'sync '(helm helm-c-yasnippet helm-ls-git))
 
+(defun my-helm-mini ()
+  (interactive)
+  (require 'helm-files)
+  (helm-other-buffer '(
+                       helm-source-buffers-list
+                       helm-source-recentf
+                       helm-source-file-cache
+                       helm-source-files-in-current-dir
+                       )
+                     "*helm mini*"))
+
 (eval-after-load 'helm
   '(progn
-     (global-set-key (kbd "C-;") 'helm-mini)
+     (global-set-key (kbd "C-;") 'my-helm-mini)
+     (global-set-key (kbd "M-;") 'helm-ls-git-ls)
      (global-set-key (kbd "C-:") 'helm-M-x)
      (global-set-key (kbd "M-x") 'helm-M-x)
      (global-set-key (kbd "M-y") 'helm-show-kill-ring)
