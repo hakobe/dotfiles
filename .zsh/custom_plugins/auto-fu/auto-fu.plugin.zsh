f2bcffe4f382a145db3c2dd3089eec4ec993cdd3
hakobe <hakobe@gmail.com>
2014-03-30 10:46:09 +0900

    taraoさんバージョン

--- .zsh/custom_plugins/auto-fu/auto-fu.plugin.zsh
+++ .zsh/custom_plugins/auto-fu/auto-fu.plugin.zsh
@@ -1,5 +1,40 @@
 source ~/.zsh/custom_plugins/auto-fu/auto-fu.zsh.git/auto-fu.zsh
-function zle-line-init () {
-    auto-fu-init
-}
+# initialization and options
+function zle-line-init () { auto-fu-init }
 zle -N zle-line-init
+zstyle ':auto-fu:highlight' input bold
+zstyle ':auto-fu:highlight' completion fg=white
+zstyle ':auto-fu:var' postdisplay ''
+
+# afu+cancel
+function afu+cancel () {
+    afu-clearing-maybe
+    ((afu_in_p == 1)) && { afu_in_p=0; BUFFER="$buffer_cur"; }
+}
+function bindkey-advice-before () {
+    local key="$1"
+    local advice="$2"
+    local widget="$3"
+    [[ -z "$widget" ]] && {
+        local -a bind
+        bind=(`bindkey -M main "$key"`)
+        widget=$bind[2]
+    }
+    local fun="$advice"
+    if [[ "$widget" != "undefined-key" ]]; then
+        local code=${"$(<=(cat <<"EOT"
+            function $advice-$widget () {
+                zle $advice
+                zle $widget
+            }
+            fun="$advice-$widget"
+EOT
+        ))"}
+        eval "${${${code//\$widget/$widget}//\$key/$key}//\$advice/$advice}"
+    fi
+    zle -N "$fun"
+    bindkey -M afu "$key" "$fun"
+}
+bindkey-advice-before "^G" afu+cancel
+bindkey-advice-before "^[" afu+cancel
+bindkey-advice-before "^J" afu+cancel afu+accept-line
--- .zshrc
+++ .zshrc
@@ -32,7 +32,7 @@ ZSH_THEME="cloud"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git git-extras go ruby perl rbenv tmux vagrant brew cpanm bundler node npm osx sbt scala)
+plugins=(auto-fu git git-extras go ruby perl rbenv tmux vagrant brew cpanm bundler node npm osx sbt scala)
 
 source $ZSH/oh-my-zsh.sh
 
