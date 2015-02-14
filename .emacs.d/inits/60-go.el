e4969e14df7e66a52b6e86a5489224cdbf9c30b9
hakobe <hakobe@gmail.com>
2015-02-14 23:13:00 +0900

    flycheck-mode

--- .emacs.d/inits/60-go.el
+++ .emacs.d/inits/60-go.el
@@ -3,6 +3,7 @@
           '(lambda ()
             (require 'go-autocomplete)
             (require 'auto-complete-config)
+            (flycheck-mode 1)
             (set (make-local-variable 'c-basic-offset) 4)
             (set (make-local-variable 'indent-tabs-mode) t)))
 
--- .emacs.d/inits/63-js.el
+++ .emacs.d/inits/63-js.el
@@ -1,3 +1,4 @@
 (add-hook 'js-mode-hook
           '(lambda ()
+            (flycheck-mode 1)
             (set (make-local-variable 'js-indent-level) 2)))
