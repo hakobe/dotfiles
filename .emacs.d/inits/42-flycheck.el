32c2d45bc57d44cbaeb62553a6560060a6848b94
hakobe <hakobe@gmail.com>
2015-02-14 19:40:12 +0900

    不要な定義消す

--- .emacs.d/inits/42-flycheck.el
+++ .emacs.d/inits/42-flycheck.el
@@ -1,8 +1,1 @@
-(add-to-list 'el-get-sources
-    '(:name flycheck
-           :type github
-           :pkgname "lunaryorn/flycheck"
-           :description "On-the-fly syntax checking extension"
-           :depends (s dash cl-lib f pkg-info)))
-
 (el-get 'sync 'flycheck)
