65b18bb31c5e4ee06e0652c8fa0e9f0d1748457d
hakobe <hakobe@gmail.com>
2015-05-05 23:58:09 +0900

    magit-init が本体とかぶってた

--- .emacs.d/inits/41-magit.el
+++ .emacs.d/inits/41-magit.el
@@ -1,8 +1,8 @@
-(defun magit-init ()
+(defun magit-my-init ()
   ; stashはたくさんためるので表示しない
   (remove-hook 'magit-status-sections-hook 'magit-insert-stashes))
 
 (el-get-bundle! magit
-  (setq magit-last-seen-setup-instructions "1.4.0")
+  ;(setq magit-last-seen-setup-instructions "1.4.0")
   (add-hook 'magit-mode-hook
-            '(lambda () (magit-init))))
+            '(lambda () (magit-my-init))))
