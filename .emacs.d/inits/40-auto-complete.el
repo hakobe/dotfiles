31ef5eaa3dd6aaa4b31f7c0fae13cc9f4d62fac7
hakobe <hakobe@gmail.com>
2015-09-16 12:58:41 +0900

    auto-completeの振舞い変更

--- .emacs.d/inits/40-auto-complete.el
+++ .emacs.d/inits/40-auto-complete.el
@@ -1,4 +1,5 @@
 (el-get-bundle auto-complete
+  (setq-default ac-sources '(ac-source-yasnippet ac-source-filename ac-source-words-in-buffer ac-source-words-in-same-mode-buffers ac-source-files-in-current-dir ac-source-dictionary ac-source-abbrev))
   (global-auto-complete-mode t)
   (setq ac-auto-show-menu 0.3)
   (setq ac-use-menu-map t)
