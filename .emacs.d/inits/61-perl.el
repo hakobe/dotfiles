c008c1eaf761ba76e92c4692e886d3ea7566f735
hakobe <hakobe@gmail.com>
2015-12-06 14:44:18 +0900

    Perlのテストを良い感じに実行できるよう

--- .emacs.d/inits/61-perl.el
+++ .emacs.d/inits/61-perl.el
@@ -4,6 +4,41 @@
 (setq auto-mode-alist (cons '("\\.psgi$" . cperl-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("cpanfile$" . cperl-mode) auto-mode-alist))
 
+(defun run-perl-test ()
+  "test実行します"
+  (interactive)
+  (compile (format "cd %s; carton exec -- perl %s" (vc-git-root default-directory) (buffer-file-name (current-buffer)))))
+
+(defun run-perl-method-test ()
+  (interactive)
+  (let (
+        (command compile-command)
+        (test-method nil))
+    (save-excursion
+      (when (or
+             (re-search-backward "\\bsub\s+\\([_[:alpha:]]+\\)\s*:\s*Test" nil t)
+             (re-search-forward "\\bsub\s+\\([_[:alpha:]]+\\)\s*:\s*Test" nil t))
+        (setq test-method (match-string 1))))
+    (if test-method
+        (compile (format "cd  %s; TEST_METHOD=%s carton exec -- perl %s"
+                         (vc-git-root default-directory)
+                         (shell-quote-argument test-method)
+                         (buffer-file-name (current-buffer))))
+      (let ((a 1))
+        (save-excursion
+          (when (or
+                 (re-search-backward "^subtest\s+['\"]?\\([^'\"\s]+\\)['\"]?\s*=>\s*sub" nil t)
+                 (re-search-foreward "^subtest\s+['\"]?\\([^'\"\s]+\\)['\"]?\s*=>\s*sub" nil t))
+            (setq test-method (match-string 1))))
+        (if test-method
+            (compile (format "cd  %s; SUBTEST_FILTER=%s carton exec -- perl %s"
+                             (vc-git-root default-directory)
+                             (shell-quote-argument test-method)
+                             (buffer-file-name (current-buffer)))))
+        (message "not match")
+        ))))
+
+
 (defun flycheck-perl-init ()
     (require 'flycheck)
     (require 'vc-git)
@@ -30,6 +65,10 @@
              (set (make-local-variable 'cperl-tab-always-indent) t)
              (set (make-local-variable 'cperl-indent-parens-as-block) t)
              (set (make-local-variable 'evil-shift-width) 4)
+
+             (define-key cperl-mode-map [(super T)] 'run-perl-test)
+             (define-key cperl-mode-map [(super t)] 'run-perl-method-test)
+
              (yas-minor-mode 1)
              (flycheck-perl-init)
              (flycheck-mode 1)
