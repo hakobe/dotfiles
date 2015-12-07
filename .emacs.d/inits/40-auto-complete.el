1951de70d8c87835d5056501393d57304f146df5
hakobe <hakobe@gmail.com>
2015-12-07 11:44:52 +0900

    auto-complete

--- .emacs.d/inits/40-auto-complete.el
+++ .emacs.d/inits/40-auto-complete.el
@@ -1,8 +1,15 @@
 (el-get-bundle auto-complete
-  (setq-default ac-sources '(ac-source-yasnippet ac-source-filename ac-source-words-in-buffer ac-source-words-in-same-mode-buffers ac-source-files-in-current-dir ac-source-dictionary ac-source-abbrev))
-  (global-auto-complete-mode t)
+  :post-init (progn
+                (add-to-list 'ac-dictionary-directories
+                              (expand-file-name "dict" default-directory))))
+  (setq-default ac-sources
+                '(ac-source-yasnippet ac-source-filename ac-source-words-in-buffer ac-source-words-in-same-mode-buffers ac-source-files-in-current-dir ac-source-dictionary ac-source-abbrev))
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup) ;; elispの設定だけここにちょろっと書いとく
+
   (setq ac-auto-show-menu 0.3)
   (setq ac-use-menu-map t)
+
+  (global-auto-complete-mode t)
 
   (with-eval-after-load-feature 'auto-complete
     (define-key ac-menu-map (kbd "\C-n") 'ac-next)
