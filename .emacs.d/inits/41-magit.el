3510008c68f55ac9712d2807b18cecd11a66f982
hakobe <hakobe@gmail.com>
2015-12-07 11:45:12 +0900

    keybind

--- .emacs.d/inits/41-magit.el
+++ .emacs.d/inits/41-magit.el
@@ -4,5 +4,6 @@
 
 (el-get-bundle! magit
   (setq magit-last-seen-setup-instructions "1.4.0")
+  (global-set-key (kbd "C-c s") 'magit-status)
   (add-hook 'magit-mode-hook
             '(lambda () (magit-my-init))))
