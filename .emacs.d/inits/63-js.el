135eb37723450d88e89519b4b5d8be5f0f7f9c14
hakobe <hakobe@gmail.com>
2016-01-18 11:29:18 +0900

    js2-mode

--- .emacs.d/inits/63-js.el
+++ .emacs.d/inits/63-js.el
@@ -1,4 +1,6 @@
-(add-hook 'js-mode-hook
+(el-get-bundle js2-mode)
+(setq auto-mode-alist (cons '("\\.js$" . js2-mode) auto-mode-alist))
+(add-hook 'js2-mode-hook
           '(lambda ()
             (flycheck-mode 1)
             (set (make-local-variable 'js-indent-level) 4)
