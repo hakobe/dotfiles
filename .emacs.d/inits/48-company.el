9ab9c68cb92a3e8160445fabdaf00cceab4fb617
hakobe <hakobe@gmail.com>
2016-02-06 14:24:07 +0900

    companyデフォルトで起動するのやめる

--- .emacs.d/inits/48-company.el
+++ .emacs.d/inits/48-company.el
@@ -1,11 +1,15 @@
 (el-get-bundle company-mode
-  (global-company-mode +1)
+  ; (global-company-mode +1)
 
   (with-eval-after-load-feature 'company
-    (global-set-key (kbd "C-M-i") 'company-complete)
     (setq company-idle-delay 0) ; デフォルトは0.5
-    (setq company-minimum-prefix-length 2) ; デフォルトは4
+    (setq company-minimum-prefix-length 1) ; デフォルトは4
     (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+
+    (define-key company-active-map (kbd "M-n") nil)
+    (define-key company-active-map (kbd "M-p") nil)
+    (define-key company-search-map (kbd "M-n") nil)
+    (define-key company-search-map (kbd "M-p") nil)
 
     ;; C-n, C-pで補完候補を次/前の候補を選択
     (define-key company-active-map (kbd "C-n") 'company-select-next)
@@ -13,12 +17,14 @@
     (define-key company-search-map (kbd "C-n") 'company-select-next)
     (define-key company-search-map (kbd "C-p") 'company-select-previous)
 
+    ;;; C-hがデフォルトでドキュメント表示にmapされているので、文字を消せるようにmapを外す
+    (define-key company-active-map (kbd "C-h") nil)
+
     ;; C-sで絞り込む
     (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
 
-    ;; TABで候補を設定
-    (define-key company-active-map (kbd "C-i") 'company-complete-selection)
-
-    ;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
-    (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
+    ;;; 1つしか候補がなかったらtabで補完、複数候補があればtabで次の候補へ行くように
+    (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
+    ;;; ドキュメント表示
+    (define-key company-active-map (kbd "M-d") 'company-show-doc-buffer)
 ))
