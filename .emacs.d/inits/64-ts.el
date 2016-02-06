f0a95933c74b1c24a0ca0b68b9fbf0be0f2777b1
hakobe <hakobe@gmail.com>
2016-02-06 13:20:48 +0900

    ts setting

--- .emacs.d/inits/64-ts.el
+++ .emacs.d/inits/64-ts.el
@@ -1,10 +1,13 @@
-(el-get-bundle carltonf/emacs-tss
-  :depends (auto-complete json-mode log4e yaxception))
+(el-get-bundle ananthakumaran/typescript.el)
+(el-get-bundle ananthakumaran/tide)
 
 (autoload 'typescript-mode "typescript" nil t)
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
 
 (add-hook 'typescript-mode-hook
         '(lambda ()
-            (require 'tss)
-            (set (make-local-variable 'typescript-indent-level) 2)))
+            (set (make-local-variable 'typescript-indent-level) 2)
+            (require 'tide)
+            (tide-setup)
+            (flycheck-mode t)
+            (eldoc-mode t)))
