590e72d5b6fb0a03dae0402f1b8328de9e2dc7a0
hakobe <hakobe@gmail.com>
2016-02-20 23:27:08 +0900

    ac-source-dictionarやめる

--- .emacs.d/inits/40-auto-complete.el
+++ .emacs.d/inits/40-auto-complete.el
@@ -3,7 +3,7 @@
                 (add-to-list 'ac-dictionary-directories
                               (expand-file-name "dict" default-directory))))
   (setq-default ac-sources
-                '(ac-source-yasnippet ac-source-filename ac-source-words-in-buffer ac-source-words-in-same-mode-buffers ac-source-files-in-current-dir ac-source-dictionary ac-source-abbrev))
+                '(ac-source-yasnippet ac-source-filename ac-source-words-in-buffer ac-source-words-in-same-mode-buffers ac-source-files-in-current-dir ac-source-abbrev))
   (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup) ;; elispの設定だけここにちょろっと書いとく
 
   (setq ac-auto-show-menu 0.3)
