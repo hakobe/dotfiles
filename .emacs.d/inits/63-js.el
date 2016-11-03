f03d77d99ecb54c1de0672ff9438fb8fe3e582f0
hakobe <hakobe@gmail.com>
2016-11-04 00:17:08 +0900

    npm bin内のeslintを使う

--- .emacs.d/inits/63-js.el
+++ .emacs.d/inits/63-js.el
@@ -3,5 +3,9 @@
 (add-hook 'js2-mode-hook
           '(lambda ()
             (flycheck-mode 1)
+            (let* ((local-bin (replace-regexp-in-string
+                      "[\r\n]+$" "" (shell-command-to-string "npm bin"))))
+                (flycheck-set-checker-executable 'javascript-eslint (concat local-bin "/eslint")))
+            (set (make-local-variable 'flycheck-checker) 'javascript-eslint)
             (set (make-local-variable 'js-indent-level) 4)
             (set (make-local-variable 'evil-shift-width) 4)))
